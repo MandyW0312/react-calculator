82a882fad8ccfa6ca5d29c89e2f49cd5a27c711c
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-24 09:42:49 -0500

    push

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,30 +1,180 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function Calculator() {
+  const [number, setNumber] = useState(0)
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [total, setTotal] = useState(0)
+
   return (
     <main>
       <div className="calculator">
-        <div className="display">0</div>
+        <div className="display">{number}</div>
         <div className="buttons">
-          <button className="button fn">AC</button>
-          <button className="button fn">&plusmn;</button>
-          <button className="button fn">%</button>
-          <button className="button op">&divide;</button>
-          <button className="button">7</button>
-          <button className="button">8</button>
-          <button className="button">9</button>
-          <button className="button op">&times;</button>
-          <button className="button">4</button>
-          <button className="button">5</button>
-          <button className="button">6</button>
-          <button className="button op">&minus;</button>
-          <button className="button">1</button>
-          <button className="button">2</button>
-          <button className="button">3</button>
-          <button className="button op">+</button>
-          <button className="button x2">0</button>
+          <button
+            className="button fn"
+            onClick={function () {
+              setNumber(0)
+              setFirstNumber(0)
+              setSecondNumber(0)
+              setTotal(0)
+            }}
+          >
+            AC
+          </button>
+          <button
+            className="button fn"
+            onClick={function () {
+              setNumber(number * -1)
+            }}
+          >
+            &plusmn;
+          </button>
+          <button
+            className="button fn"
+            onClick={function () {
+              setNumber(number / 100)
+            }}
+          >
+            %
+          </button>
+          <button
+            className="button op"
+            onClick={function () {
+              setFirstNumber(number)
+              setNumber(0)
+            }}
+          >
+            &divide;
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '7')
+            }}
+          >
+            7
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '8')
+            }}
+          >
+            8
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '9')
+            }}
+          >
+            9
+          </button>
+          <button
+            className="button op"
+            onClick={function () {
+              setFirstNumber(number)
+              setNumber(0)
+            }}
+          >
+            &times;
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '4')
+            }}
+          >
+            4
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '5')
+            }}
+          >
+            5
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '6')
+            }}
+          >
+            6
+          </button>
+          <button
+            className="button op"
+            onClick={function () {
+              setFirstNumber(number)
+              setNumber(0)
+            }}
+          >
+            &minus;
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '1')
+            }}
+          >
+            1
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '2')
+            }}
+          >
+            2
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              setNumber(number + '3')
+            }}
+          >
+            3
+          </button>
+          <button
+            className="add button op"
+            onClick={function () {
+              setFirstNumber(number)
+              setNumber(0)
+            }}
+          >
+            +
+          </button>
+          <button
+            className="button x2"
+            onClick={function () {
+              setNumber(number + '0')
+            }}
+          >
+            0
+          </button>
           <button className="button">.</button>
-          <button className="button op">=</button>
+          <button
+            className="button op"
+            onClick={function () {
+              setSecondNumber(number)
+              if ('/') {
+                setTotal(firstNumber / secondNumber)
+              }
+              if ('*') {
+                setTotal(firstNumber * secondNumber)
+              }
+              if ('add') {
+                setTotal(firstNumber + secondNumber)
+              }
+              if ('-') {
+                setTotal(firstNumber - secondNumber)
+              }
+            }}
+          >
+            =
+          </button>
         </div>
       </div>
     </main>
